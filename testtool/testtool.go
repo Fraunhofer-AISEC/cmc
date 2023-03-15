a209105ff540974375cad1f15ca046a8662daadf
Simon Ott <simon.ott@aisec.fraunhofer.de>
2023-03-15 12:00:56 +0100

    testtool: simplified configuration
    This is a preparation for allowing file-based as well
    as commandline-based configuration.
    
    Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

--- testtool/testtool.go
+++ testtool/testtool.go
@@ -18,10 +18,12 @@ package main
 // Install github packages with "go get [url]"
 import (
 	"flag"
+	"fmt"
 	"os"
 	"strings"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/maps"
 
 	"github.com/Fraunhofer-AISEC/cmc/est/client"
 	"github.com/Fraunhofer-AISEC/cmc/internal"
@@ -36,6 +38,7 @@ const (
 	Dial     = 2 // Act as client to establish an attested TLS connection
 	Listen   = 3 // Act as server in etsblishing attested TLS connections
 	CaCerts  = 4 // Retrieve CA certs from EST server
+	IoTHub   = 5 // Simulate an IoT hub for Cortex-M IAS Attestation Demo
 )
 
 const (
@@ -43,91 +46,130 @@ const (
 )
 
 type Api interface {
-	generate(addr, reportFile, nonceFile string)
-	verify(addr, reportFile, resultFile, nonceFile string, ca, policies []byte)
-	dial(addr, cmcAddr string, mtls bool, ca, policies []byte)
-	listen(addr, cmcAddr string, mtls bool, ca, policies []byte)
+	generate(c *config)
+	verify(c *config)
+	dial(c *config)
+	listen(c *config)
 }
 
 var apis = map[string]Api{}
 
 var log = logrus.WithField("service", "testtool")
 
-func main() {
-	logrus.SetLevel(logrus.TraceLevel)
+type config struct {
+	Mode         string
+	Addr         string
+	CmcAddr      string
+	ReportFile   string
+	ResultFile   string
+	NonceFile    string
+	CaFile       string
+	Mtls         bool
+	PoliciesFile string
+	ApiFlag      string
 
-	modeFlag := flag.String("mode", "generate", "[generate | verify | dial  | listen ]")
+	ca       []byte
+	policies []byte
+}
+
+func getConfig() *config {
+	var err error
+
+	// configFile := flag.String("config", "", "configuration file")
+	modeFlag := flag.String("mode", "generate", "[generate | verify | dial  | listen | cacerts | iothub ]")
 	addr := flag.String("addr", "0.0.0.0:443", "server ip:port to connect to / listen on via attested tls")
 	cmcAddr := flag.String("cmc", "127.0.0.1:9955", "TCP address to connect to the cmcd API")
 	reportFile := flag.String("report", "attestation-report", "Output file for the attestation report")
 	resultFile := flag.String("result", "attestation-result.json", "Output file for the attestation result")
 	nonceFile := flag.String("nonce", "nonce", "Output file for the nonce")
-	caFile := flag.String("ca", "ca.pem", "TLS Certificate of CA / Entity that is RoT of the connector's TLS certificate")
+	caFile := flag.String("ca", "", "TLS Certificate of CA / Entity that is RoT of the connector's TLS certificate")
 	mtls := flag.Bool("mtls", false, "Performs mutual TLS with remote attestation on both sides. Only in mode tlsconn")
 	policiesFile := flag.String("policies", "", "JSON policies file for custom verification")
-	apiFlag := flag.String("api", "grpc", "API to contact the cmcd (grpc or coap)")
+	apiFlag := flag.String("api", "grpc", fmt.Sprintf("API to contact the cmcd. Possible: %v", maps.Keys(apis)))
 	flag.Parse()
 
-	var mode Mode
-	if strings.EqualFold(*modeFlag, "generate") {
-		mode = Generate
-	} else if strings.EqualFold(*modeFlag, "verify") {
-		mode = Verify
-	} else if strings.EqualFold(*modeFlag, "dial") {
-		mode = Dial
-	} else if strings.EqualFold(*modeFlag, "listen") {
-		mode = Listen
-	} else if strings.EqualFold(*modeFlag, "cacerts") {
-		mode = CaCerts
-	} else {
-		log.Fatal("Wrong mode. Possible [Generate | Verify | TLSConn]")
+	c := &config{
+		Mode:         *modeFlag,
+		Addr:         *addr,
+		CmcAddr:      *cmcAddr,
+		ReportFile:   *reportFile,
+		ResultFile:   *resultFile,
+		NonceFile:    *nonceFile,
+		CaFile:       *caFile,
+		Mtls:         *mtls,
+		PoliciesFile: *policiesFile,
+		ApiFlag:      *apiFlag,
 	}
 
-	api, ok := apis[strings.ToLower(*apiFlag)]
-	if !ok {
-		log.Fatalf("API '%v' is not implemented\n", *apiFlag)
-	}
-
-	// Get root CA certificate in PEM format
-	var ca []byte
-	var err error
-	if mode != Generate && mode != CaCerts {
-		ca, err = os.ReadFile(*caFile)
+	// Get root CA certificate in PEM format if specified
+	if c.CaFile != "" {
+		c.ca, err = os.ReadFile(c.CaFile)
 		if err != nil {
 			log.Fatalf("Failed to read file %v: %v", *caFile, err)
 		}
 	}
 
-	// Add optional policies if present
-	var policies []byte = nil
-	if *policiesFile != "" {
+	// Add optional policies if specified
+	if c.PoliciesFile != "" {
 		log.Debug("Adding specified policies")
-		policies, err = os.ReadFile(*policiesFile)
+		c.policies, err = os.ReadFile(c.PoliciesFile)
 		if err != nil {
 			log.Fatalf("Failed to read policies file: %v", err)
 		}
 	}
 
+	return c
+}
+
+func main() {
+	logrus.SetLevel(logrus.TraceLevel)
+
+	c := getConfig()
+
+	var mode Mode
+	if strings.EqualFold(c.Mode, "generate") {
+		mode = Generate
+	} else if strings.EqualFold(c.Mode, "verify") {
+		mode = Verify
+	} else if strings.EqualFold(c.Mode, "dial") {
+		mode = Dial
+	} else if strings.EqualFold(c.Mode, "listen") {
+		mode = Listen
+	} else if strings.EqualFold(c.Mode, "cacerts") {
+		mode = CaCerts
+	} else {
+		log.Errorf("Undefined mode %v", c.Mode)
+		flag.Usage()
+		return
+	}
+
+	api, ok := apis[strings.ToLower(c.ApiFlag)]
+	if !ok {
+		log.Errorf("API %v is not implemented\n", c.ApiFlag)
+		flag.Usage()
+		return
+	}
+
 	switch mode {
 	case Generate:
-		api.generate(*cmcAddr, *reportFile, *nonceFile)
+		api.generate(c)
 	case Verify:
-		api.verify(*cmcAddr, *reportFile, *resultFile, *nonceFile, ca, policies)
+		api.verify(c)
 	case Dial:
-		api.dial(*addr, *cmcAddr, *mtls, ca, policies)
+		api.dial(c)
 	case Listen:
-		api.listen(*addr, *cmcAddr, *mtls, ca, policies)
+		api.listen(c)
 	case CaCerts:
-		getCaCerts(*addr, *caFile)
+		getCaCerts(c)
 	default:
 		log.Fatalf("Unknown mode %v", mode)
 	}
 }
 
-func getCaCerts(addr, caFile string) {
+func getCaCerts(c *config) {
 	log.Info("Retrieving CA certs")
 	estclient := client.NewClient(nil)
-	certs, err := estclient.CaCerts(addr)
+	certs, err := estclient.CaCerts(c.Addr)
 	if err != nil {
 		log.Fatalf("Failed to retrieve certificates: %v", err)
 	}
@@ -136,7 +178,7 @@ func getCaCerts(addr, caFile string) {
 		log.Debugf("\t%v", c.Subject.CommonName)
 	}
 	// Store CA certificate
-	err = os.WriteFile(caFile, internal.WriteCertPem(certs[len(certs)-1]), 0644)
+	err = os.WriteFile(c.CaFile, internal.WriteCertPem(certs[len(certs)-1]), 0644)
 	if err != nil {
 		log.Fatalf("Failed to store CA certificate: %v", err)
 	}
