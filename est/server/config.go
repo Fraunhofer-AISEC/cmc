fe39a54c16ae52d3944509a5328f67bee49928c6
Simon Ott <simon.ott@aisec.fraunhofer.de>
2023-03-22 10:55:01 +0000

    est/server: Enabled config via commandline and config file
    Commandline flags supersede configuration file options if
    both are given.
    
    Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

--- est/server/config.go
+++ est/server/config.go
@@ -16,18 +16,31 @@
 package main
 
 import (
+	"crypto/ecdsa"
 	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"errors"
+	"flag"
 	"fmt"
-	"os"
-	"path"
 	"path/filepath"
 	"strings"
 
-	ar "github.com/Fraunhofer-AISEC/cmc/attestationreport"
 	"github.com/Fraunhofer-AISEC/cmc/internal"
 	log "github.com/sirupsen/logrus"
+	"golang.org/x/exp/maps"
+)
+
+var (
+	logLevels = map[string]log.Level{
+		"panic": log.PanicLevel,
+		"fatal": log.FatalLevel,
+		"error": log.ErrorLevel,
+		"warn":  log.WarnLevel,
+		"info":  log.InfoLevel,
+		"debug": log.DebugLevel,
+		"trace": log.TraceLevel,
+	}
 )
 
 type config struct {
@@ -41,61 +54,169 @@ type config struct {
 	TpmEkCertDb        string   `json:"tpmEkCertDb,omitempty"`
 	VcekOfflineCaching bool     `json:"vcekOfflineCaching,omitempty"`
 	VcekCacheFolder    string   `json:"vcekCacheFolder,omitempty"`
-	Serialization      string   `json:"serialization"`
+	LogLevel           string   `json:"logLevel"`
+
+	configDir    *string
+	signingKey   *ecdsa.PrivateKey
+	signingCerts []*x509.Certificate
+	estKey       *ecdsa.PrivateKey
+	estCerts     []*x509.Certificate
 }
 
-// Returns either the unmodified absolute path or the absolute path
-// retrieved from a path relative to a base path
-func getFilePath(p, base string) string {
-	if path.IsAbs(p) {
-		return p
-	}
-	ret, _ := filepath.Abs(filepath.Join(base, p))
-	return ret
-}
+const (
+	configFlag             = "config"
+	portFlag               = "port"
+	signingKeyFlag         = "signingkey"
+	signingCertsFlag       = "signingcerts"
+	estKeyFlag             = "estkey"
+	estCertsFlag           = "estcerts"
+	httpFolderFlag         = "httpfolder"
+	verifyEkCertFlag       = "verifyek"
+	tpmEkCertDbFlag        = "ekdb"
+	vcekOfflineCachingFlag = "vcekcaching"
+	vcekCacheFolderFlag    = "vcekfolder"
+	logFlag                = "log"
+)
 
-func readConfig(configFile string) (*config, error) {
-	data, err := os.ReadFile(configFile)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read config file '%v': %w", configFile, err)
-	}
-	config := new(config)
-	err = json.Unmarshal(data, config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+func getConfig() (*config, error) {
+	var ok bool
+	var err error
+
+	// Parse given command line flags
+	configFile := flag.String(configFlag, "", "configuration file")
+	port := flag.Int(portFlag, 0, "Port to listen on")
+	signingKey := flag.String(signingKeyFlag, "", "Path to the key used for signing certs")
+	signingCerts := flag.String(signingCertsFlag, "", "Path to the signing key cert chain")
+	estKey := flag.String(estKeyFlag, "", "TLS key for HTTPS server")
+	estCerts := flag.String(estCertsFlag, "", "Certificate chain for TLS key")
+	httpFolder := flag.String(httpFolderFlag, "", "Folder to be served")
+	verifyEkCert := flag.Bool(verifyEkCertFlag, false,
+		"Indicates whether to verify TPM EK certificate chains")
+	tpmEkCertDb := flag.String(tpmEkCertDbFlag, "", "Database for EK cert chain verification")
+	vcekOfflineCaching := flag.Bool(vcekOfflineCachingFlag, false,
+		"Indicates whether to cache downloaded AMD SNP VCEKs")
+	vcekCacheFolder := flag.String(vcekCacheFolderFlag, "", "Folder to cache AMD SNP VCEKs")
+	logLevel := flag.String(logFlag, "",
+		fmt.Sprintf("Possible logging: %v", maps.Keys(logLevels)))
+	flag.Parse()
+
+	// Create default configuration
+	c := &config{
+		Port:         9000,
+		VerifyEkCert: true,
+		LogLevel:     "info",
 	}
 
-	if !config.VerifyEkCert {
+	// Obtain custom configuration from file if specified
+	if internal.FlagPassed(configFlag) {
+		data, err := internal.GetFile(*configFile, nil)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read config file '%v': %w", configFile, err)
+		}
+		err = json.Unmarshal(data, c)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+		}
+		dir := filepath.Dir(*configFile)
+		c.configDir = &dir
+	}
+
+	// Overwrite config file configuration with given commandline arguments
+	if internal.FlagPassed(portFlag) {
+		c.Port = *port
+	}
+	if internal.FlagPassed(signingKeyFlag) {
+		c.SigningKey = *signingKey
+	}
+	if internal.FlagPassed(signingCertsFlag) {
+		c.SigningCerts = strings.Split(*signingCerts, ",")
+	}
+	if internal.FlagPassed(estKeyFlag) {
+		c.EstKey = *estKey
+	}
+	if internal.FlagPassed(estCertsFlag) {
+		c.EstCerts = strings.Split(*estCerts, ",")
+	}
+	if internal.FlagPassed(httpFolderFlag) {
+		c.HttpFolder = *httpFolder
+	}
+	if internal.FlagPassed(verifyEkCertFlag) {
+		c.VerifyEkCert = *verifyEkCert
+	}
+	if internal.FlagPassed(tpmEkCertDbFlag) {
+		c.TpmEkCertDb = *tpmEkCertDb
+	}
+	if internal.FlagPassed(vcekOfflineCachingFlag) {
+		c.VcekOfflineCaching = *vcekOfflineCaching
+	}
+	if internal.FlagPassed(vcekCacheFolderFlag) {
+		c.VcekCacheFolder = *vcekCacheFolder
+	}
+	if internal.FlagPassed(logFlag) {
+		c.LogLevel = *logLevel
+	}
+
+	// Configure the logger
+	l, ok := logLevels[strings.ToLower(c.LogLevel)]
+	if !ok {
+		flag.Usage()
+		log.Fatalf("LogLevel %v does not exist", c.LogLevel)
+	}
+	log.SetLevel(l)
+
+	// Print the parsed configuration
+	printConfig(c)
+
+	// Load specified files
+	c.signingKey, err = loadPrivateKey(c.SigningKey, c.configDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load private key: %w", err)
+	}
+
+	c.signingCerts, err = loadCertChain(c.SigningCerts, c.configDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load certificate chain: %w", err)
+	}
+
+	c.estKey, err = loadPrivateKey(c.EstKey, c.configDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load private key: %w", err)
+	}
+
+	c.estCerts, err = loadCertChain(c.EstCerts, c.configDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load certificate chain: %w", err)
+	}
+
+	if !c.VerifyEkCert {
 		log.Warn("UNSAFE: Verification of EK certificate chain turned off via config")
 	}
 
-	printConfig(config, configFile)
-
-	return config, nil
+	return c, nil
 }
 
-func printConfig(c *config, configFile string) {
-	log.Infof("Using the configuration loaded from %v:", configFile)
-	log.Infof("\tPort                : %v", c.Port)
-	log.Infof("\tSigning Key File    : %v", getFilePath(c.SigningKey, filepath.Dir(configFile)))
-	log.Infof("\tSigning Certificates:")
+func printConfig(c *config) {
+	log.Debugf("Using the following configuration:")
+	log.Debugf("\tPort                : %v", c.Port)
+	log.Debugf("\tSigning Key File    : %v", c.SigningKey)
+	log.Debugf("\tSigning Certificates:")
 	for i, c := range c.SigningCerts {
-		log.Infof("\t\tCert %v: %v", i, c)
+		log.Debugf("\t\tCert %v: %v", i, c)
 	}
-	log.Infof("\tEST Key File        : %v", getFilePath(c.HttpFolder, filepath.Dir(configFile)))
-	log.Infof("\tEST Certificates    : ")
+	log.Debugf("\tEST Key File        : %v", c.EstKey)
+	log.Debugf("\tEST Certificates    : ")
 	for i, c := range c.EstCerts {
-		log.Infof("\t\tCert %v: %v", i, c)
+		log.Debugf("\t\tCert %v: %v", i, c)
 	}
-	log.Infof("\tFolders to be served: %v", getFilePath(c.HttpFolder, filepath.Dir(configFile)))
-	log.Infof("\tVerify EK Cert      : %v", c.VerifyEkCert)
-	log.Infof("\tTPM EK DB           : %v", getFilePath(c.TpmEkCertDb, filepath.Dir(configFile)))
-	log.Infof("\tVCEK Offline Caching: %v", c.VcekOfflineCaching)
-	log.Infof("\tVCEK Cache Folder   : %v", getFilePath(c.VcekCacheFolder, filepath.Dir(configFile)))
-	log.Infof("\tSerialization       : %v", c.Serialization)
+	log.Debugf("\tFolders to be served: %v", c.HttpFolder)
+	log.Debugf("\tVerify EK Cert      : %v", c.VerifyEkCert)
+	log.Debugf("\tTPM EK DB           : %v", c.TpmEkCertDb)
+	log.Debugf("\tVCEK Offline Caching: %v", c.VcekOfflineCaching)
+	log.Debugf("\tVCEK Cache Folder   : %v", c.VcekCacheFolder)
+	log.Debugf("\tLog Level           : %v", c.LogLevel)
 }
 
-func loadCertChain(certChainFiles []string, configFile string) ([]*x509.Certificate, error) {
+func loadCertChain(certChainFiles []string, base *string) ([]*x509.Certificate, error) {
 
 	certChain := make([]*x509.Certificate, 0)
 
@@ -105,9 +226,9 @@ func loadCertChain(certChainFiles []string, configFile string) ([]*x509.Certific
 
 	// Load certificate chain
 	for _, f := range certChainFiles {
-		pem, err := os.ReadFile(getFilePath(f, filepath.Dir(configFile)))
+		pem, err := internal.GetFile(f, base)
 		if err != nil {
-			return nil, fmt.Errorf("failed to read file %v: %w", f, err)
+			return nil, fmt.Errorf("failed to get file: %w", err)
 		}
 		cert, err := internal.ParseCert(pem)
 		if err != nil {
@@ -119,14 +240,20 @@ func loadCertChain(certChainFiles []string, configFile string) ([]*x509.Certific
 	return certChain, nil
 }
 
-func getSerializer(s string) (ar.Serializer, error) {
-	if strings.EqualFold(s, "JSON") {
-		log.Info("Using JSON/JWS as serialization interface")
-		return ar.JsonSerializer{}, nil
-	} else if strings.EqualFold(s, "CBOR") {
-		log.Info("Using CBOR/COSE as serialization interface")
-		return ar.CborSerializer{}, nil
-	} else {
-		return nil, fmt.Errorf("serializer %v not supported (only 'json' and 'cbor')", s)
+func loadPrivateKey(caPrivFile string, base *string) (*ecdsa.PrivateKey, error) {
+
+	// Read private pem-encoded key and convert it to a private key
+	privBytes, err := internal.GetFile(caPrivFile, base)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get file: %w", err)
 	}
+
+	privPem, _ := pem.Decode(privBytes)
+
+	priv, err := x509.ParseECPrivateKey(privPem.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse private key: %w", err)
+	}
+
+	return priv, nil
 }
