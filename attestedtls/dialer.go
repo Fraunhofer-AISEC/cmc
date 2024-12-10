3c801b3c6be05b0a1621b35a60631b9bde6d92d2
Simon Ott <simon.ott@aisec.fraunhofer.de>
2024-12-10 17:04:11 +0000

    attestedtls/http: logging and fixes
    Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

--- attestedtls/dialer.go
+++ attestedtls/dialer.go
@@ -30,6 +30,8 @@ import (
 // before returning the established connection.
 func Dial(network string, addr string, config *tls.Config, moreConfigs ...ConnectionOption[CmcConfig]) (*tls.Conn, error) {
 
+	log.Tracef("Dialing TLS server %v:%v", network, addr)
+
 	if config == nil {
 		return nil, errors.New("failed to dial. TLS configuration not provided")
 	}
@@ -47,10 +49,12 @@ func Dial(network string, addr string, config *tls.Config, moreConfigs ...Connec
 		return nil, fmt.Errorf("failed to establish tls connection: %w. %v", err, details)
 	}
 
+	log.Trace("Connection established, generating channel bindings")
 	cs := conn.ConnectionState()
 	if !cs.HandshakeComplete {
 		return nil, errors.New("internal error: handshake not complete")
 	}
+
 	log.Trace("TLS handshake complete, generating channel bindings")
 	chbindings, err := cs.ExportKeyingMaterial("EXPORTER-Channel-Binding", nil, 32)
 	if err != nil {
