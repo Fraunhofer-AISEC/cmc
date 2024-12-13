7be5a4a263866ad9a10a9a5b4ef308301eb9bebb
Simon Ott <simon.ott@aisec.fraunhofer.de>
2024-12-13 13:38:24 +0000

    example-setup: updated scripts
    Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

--- example-setup/utils.sh
+++ example-setup/utils.sh
@@ -15,20 +15,3 @@ extendarr() {
   # Add new value
   json="$(echo "${json}" | jq ".${key} += [${param}]")"
 }
-
-
-# Updates the JSON key given in the first argument with the value given
-# in the second argument. Numbers must be \"\" quoted, if they should
-# be treated as a JSON string
-setjson() {
-  if [[ $2 == \"*\" || $2 == \'*\' ]]; then
-    # Parameter is string in quotes, use quotes
-    json="$(echo "${json}" | jq ".$1 = $2")"
-  elif [[ $2 =~ ^[0-9]+$ || $2 == "true" || $2 == "false" ]]; then
-    # Parameter is number or boolean, do not use quotes
-    json="$(echo "${json}" | jq ".$1 = $2")"
-  else
-    # Parameter is string, use quotes
-    json="$(echo "${json}" | jq ".$1 = \"$2\"")"
-  fi
-}
