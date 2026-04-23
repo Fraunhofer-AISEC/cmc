c22053f59317cd05d6ca3481f50e0162c5ce95cf
Simon Ott <simon.ott@aisec.fraunhofer.de>
2026-04-23 06:45:12 +0000

    attestationreport/cbor: use strings as interface map keys
    Signed-off-by: Simon Ott <simon.ott@aisec.fraunhofer.de>

--- cmcctl/socket.go
+++ cmcctl/socket.go
@@ -126,7 +126,7 @@ func (a SocketApi) verify(c *config) error {
 	err = pub.Publish(c.PublishResults, c.PublishOcsf, c.PublishNetwork, c.publishToken, c.ResultFile, resp.Result,
 		c.rootCas, c.AllowSystemCerts, c.publishClientCert)
 	if err != nil {
-		return fmt.Errorf("failed to save result: %w", err)
+		return fmt.Errorf("failed to publish result: %w", err)
 	}
 
 	return nil
